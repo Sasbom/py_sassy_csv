7342f73c83d07f714cfc0b0496219acce9f53390
Sasbom <saskervg@live.nl>
2025-04-16 10:36:10 +0200

    Pilcrow is not utf-8 safe, using backslash by default.

--- SassyCSV/Util.hpp
+++ SassyCSV/Util.hpp
@@ -55,7 +55,7 @@ void rightpad_string(std::string& string, std::size_t desired_length) {
     }
 }
 
-void exclude_char_string(std::string& string, char c = '\n', char const * r = "¶") {
+void exclude_char_string(std::string& string, char c = '\n', char const * r = "\\") {
     while (string.find(c) != string.npos) {
         auto pos = string.find(c);
         string.replace(string.begin() + pos, string.begin() + pos + 1, r);
